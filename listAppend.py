c0d6fce22d3d70fd91c4dc601addbb8f8efe7951
Erwin Dwi Susanto <90387089+erwindwii@users.noreply.github.com>
2021-09-09 20:02:44 +0700

    Create listAppend.py

---
+++ listAppend.py
@@ -0,0 +1,9 @@
+#menambahkan isi list dengan metode append.
+cs = ["Anggun", "Dian", "Agung", "Adi", "Adam"]
+print("list yang tersedia", cs)
+i = str(input("ketik untuk menambahkan list: "))
+cs.append(i)
+print("daftar list yang sudah di tambahkan :",cs)
+
+
+
