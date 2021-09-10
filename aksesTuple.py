3a1d9f09300b990b54cc425453dd89a88d444eb6
Erwin Dwi Susanto <90387089+erwindwii@users.noreply.github.com>
2021-09-10 09:08:20 +0700

    Create aksesTuple.py

---
+++ aksesTuple.py
@@ -0,0 +1,6 @@
+#mengakses nilai tuple.
+tup = ('co','di','ng','ku','y')
+print("co di ng ku y, ketik dua huruf yang ingin ditampilkan")
+start = int(input("index minimum :"))
+finish = int(input("index maximum :"))
+print(tup[start:finish])
